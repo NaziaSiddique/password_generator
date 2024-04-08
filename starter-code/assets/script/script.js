40cdeb89dd30af0b041cd24a39d375972036d1bf
NaziaSiddique <nazia.siddique04@gmail.com>
2024-04-08 10:36:35 +0100

    Selecting all elements from the HTML

---
+++ starter-code/assets/script/script.js
@@ -0,0 +1,11 @@
+// Elements
+const slider = document.querySelector('input[type=range]');
+const sliderValue = document.querySelector('.value');
+const form = document.querySelector('form');
+const generateBtn = document.querySelector('button');
+const constraints = document.querySelectorAll('input[name=options]');
+const output = document.querySelector('.result__text-field');
+const copyIcon = document.querySelector('.password-generator__result > img');
+const copyText = document.querySelector('.copied');
+const passwordState = document.querySelector('.password-strength__state');
+const passwordStrength = document.getElementById('password-strength');
