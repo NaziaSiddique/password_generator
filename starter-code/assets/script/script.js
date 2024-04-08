822e55a78d6d69ed01aaa7632a166263f2d7a94e
NaziaSiddique <nazia.siddique04@gmail.com>
2024-04-08 10:50:22 +0100

    Created an object to contain teh character sets

--- starter-code/assets/script/script.js
+++ starter-code/assets/script/script.js
@@ -9,3 +9,11 @@ const copyIcon = document.querySelector('.password-generator__result > img');
 const copyText = document.querySelector('.copied');
 const passwordState = document.querySelector('.password-strength__state');
 const passwordStrength = document.getElementById('password-strength');
+
+// Character sets
+const charSets = {
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "0123456789",
+  symbols: "£$&()*+[]@#^-_!?"
+};
